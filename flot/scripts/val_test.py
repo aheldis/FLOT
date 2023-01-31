b00c326cefce517bec0eeb5273d972a1c5bafa10
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2023-01-31 13:46:29 +0330

    attack all

--- flot/scripts/val_test.py
+++ flot/scripts/val_test.py
@@ -60,7 +60,7 @@ def compute_epe(est_flow, batch):
 def fgsm_attack(image, epsilon, data_grad):
     sign_data_grad = data_grad.sign()
     perturbed_image = image + epsilon*sign_data_grad
-    perturbed_image = torch.clamp(perturbed_image, 0, 255)
+    # perturbed_image = torch.clamp(perturbed_image, 0, 255)
     return perturbed_image
 
 
